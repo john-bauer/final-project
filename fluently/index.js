7a87e2b553ddb7e15ee5314e94f4927117c0852d
john bauer <johnbauer12693@gmail.com>
2017-06-18 13:05:11 -0400

    set up environment, rendered ejs

--- fluently/index.js
+++ fluently/index.js
@@ -1,11 +1,14 @@
-/*===============================
-          DEPENDENCIES
-================================*/
+/********************************
+********* ENVIRONMENT ***********
+********************************/
+const express = require('express');
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const path = require('path');
 const env = require('dotenv').config();
 const watson = require('watson-developer-cloud');
 const language_translator = watson.language_translator({
@@ -15,18 +18,41 @@ const language_translator = watson.language_translator({
   version: "v2"
 });
 
-/*================================
-            ROUTES
-================================*/
+/*========== STATIC FILE ==========*/
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res){
-  res.send('hello express');
+/*========== VIEWS SETUP ==========*/
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+/*============ LOGGER =============*/
+app.use(logger('dev'));
+
+/*= BODY PARSER & METHOD OVERRIDE =*/
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+
+/*============ ROUTES =============*/
+app.get('/', function(req, res) {
+  res.render('index', {
+    message: 'Hello EJS',
+    documentTitle: 'Fluently',
+  });
 });
 
-/*===============================
-        SERVER LISTENING
-===============================*/
+
+/*======== SERVER LISTENING ======*/
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, function(){
+http.listen(PORT, function(){
   console.log(`listening on port ${PORT}`);
 });
+
+/*********************************
+**********************************/
+
+
+/*======== CATCHING 404 =========*/
+app.get('*', function(req, res){
+  res.status(404).send({message: 'oops! Not found.'});
+})
