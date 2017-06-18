8903228fae7fa148d1b0b390100174c81b890cb5
john bauer <johnbauer12693@gmail.com>
2017-06-18 11:45:03 -0400

    required dependencies, started server, added watson authentication

---
+++ fluently/index.js
@@ -0,0 +1,24 @@
+/*===============================
+          DEPENDENCIES
+================================*/
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const env = require('dotenv').config();
+const watson = require('watson-developer-cloud');
+const language_translator = watson.language_translator({
+  url: "https://gateway.watsonplatform.net/language-translator/api",
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  version: "v2"
+});
+
+/*===============================
+        SERVER LISTENING
+===============================*/
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function(){
+  console.log(`listening on port ${PORT}`);
+});
